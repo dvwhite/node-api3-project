29aa4641ee233bbedcdbf3ff02adf1bea145bd32
David White <dwhite0627@gmail.com>
2020-04-08 20:46:14 -0400

    Add routing to /users, add custom logger middleware to server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
-
+const cors = require("cors");
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+// Add middleware
+server.use(express.json());
+server.use(cors());
+server.use(logger);
+
+// Routes
+const userRoutes = require('./users/userRouter.js');
+server.use('/users', userRoutes);
 
 //custom middleware
 
-// Logger middleware
+// Logger
 function logger(req, res, next) {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
@@ -19,6 +24,8 @@ function logger(req, res, next) {
   next();
 }
 
-
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
