73a812e11505c939d99dfdcf5cc406607dc4635b
David White <dwhite0627@gmail.com>
2020-04-09 23:11:37 -0400

    Add dotenv, prepare for deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// code away!
+require('dotenv').config()
+
 const server = require('./server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
